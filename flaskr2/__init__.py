c9eff2849c856725701c35820dcca5441c02de09
Michigety <bizanlil94@gmail.com>
2025-01-09 16:58:08 +0900

    TRY: Flask Tutorial in flaskr2

--- flaskr2/__init__.py
+++ flaskr2/__init__.py
@@ -2,16 +2,33 @@ import os
 
 from flask import Flask
 
-BASEDIR = os.path.dirname(__file__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
 
-def create_app():
-    app = Flask(__name__)
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
 
-    if os.path.exists(os.path.join(BASEDIR, 'config.py')):
-        app.config.from_pyfile('config.py')
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-    @app.route('/')
-    def index():
-        return app.config['SECRET_KEY']
+    # @app.route('/hello')
+    # def index():
+    #     return 'Hello, World!'
+
+    from flaskr2 import db
+    db.init_app(app)
+
+    from flaskr2 import auth, blog
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
